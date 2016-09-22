4c2232914c76470586487dd5b3517b1c9ccbdc61
Florian Corbel <legolgo@laposte.net>
2016-09-22 21:16:52 +0200

    Divers

--- blocs/content-product_caracteristics.php
+++ blocs/content-product_caracteristics.php
@@ -1,12 +1,26 @@
 <?php
   $tags = get_the_terms($post->ID, 'product_tag');
 
+  function show_tag_infos($tags, $tag_slug) {
+    if ($tags[$tag_slug]) {
+      $description = htmlspecialchars($tags[$tag_slug]->description, ENT_QUOTES);
+      $id = $tags[$tag_slug]->term_taxonomy_id;
+      $with_thumbnail = has_term_thumbnail($id) ? "with_thumbnail" : "";
+      echo "<span class='tag_info " . $with_thumbnail . "' data-description='" . $description . "'>";
+      if ($with_thumbnail) {
+        the_term_thumbnail($id, 'picto-thumbnail');
+      } else {
+        echo $tags[$tag_slug]->name;
+      }
+      // print_r($tags[$tag_slug]);
+      echo "</span>";
+    }
+  }
+
   function show_facilite_entretien($tags) {
     if ($tags["peu-darrosage"] || $tags["pas-de-taille"]) {
       echo "<li id='entretien'>Entretien:";
-      if ($tags["peu-darrosage"]) {
-        echo "<span>peu d'arrosage</span>";
-      }
+      show_tag_infos($tags, "peu-darrosage");
       echo "</li>";
     }
   }
@@ -14,18 +28,10 @@
   function show_lieu($tags) {
     if ($tags["a-lombre"] || $tags["pour-la-terrasse"] || $tags["pour-le-balcon"] || $tags["pour-un-talus"]) {
       echo "<li id='lieu'>Lieu:";
-      if ($tags["a-lombre"]) {
-        echo "<span>a l'ombre</span>";
-      }
-      if ($tags["pour-la-terrasse"]) {
-        echo "<span>terrasse</span>";
-      }
-      if ($tags["pour-le-balcon"]) {
-        echo "<span>balcon</span>";
-      }
-      if ($tags["pour-un-talus"]) {
-        echo "<span>talus</span>";
-      }
+      show_tag_infos($tags, "a-lombre");
+      show_tag_infos($tags, "pour-la-terrasse");
+      show_tag_infos($tags, "pour-le-balcon");
+      show_tag_infos($tags, "pour-un-talus");
       echo "</li>";
     }
   }
--- src/php/theme_support.php
+++ src/php/theme_support.php
@@ -4,6 +4,10 @@ if (function_exists('add_theme_support')) {
   // Add Menu Support
   add_theme_support('menus');
 
+  // Add thumbnail theme support
+  add_theme_support('post-thumbnails');
+  add_image_size('picto-thumbnail', 80, 80, true);
+
   // Add custom logo
   add_theme_support('custom-logo');
 
